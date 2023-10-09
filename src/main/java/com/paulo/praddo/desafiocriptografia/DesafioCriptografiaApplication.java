9afcd5985e716ac067790491290eb3623bab2a32
unknown <paulopraddo011@gmail.com>
2023-10-09 14:16:51 -0300

    first commit

---
+++ src/main/java/com/paulo/praddo/desafiocriptografia/DesafioCriptografiaApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.praddo.desafiocriptografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCriptografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCriptografiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulo/praddo/desafiocriptografia/DesafioCriptografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.praddo.desafiocriptografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCriptografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
